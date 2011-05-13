87caf27ceb627c7731a61cb81088bf3906cb9d6b
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2011-05-13 12:59:17 +0200

    Port script to use RDF::Helper

--- kommunesentre-geonames.pl
+++ kommunesentre-geonames.pl
@@ -6,6 +6,20 @@ binmode(STDOUT, ":utf8");
 
 use URI;
 use Web::Scraper;
+use RDF::Helper '1.99_02';
+my $rdf = RDF::Helper->new(
+      namespaces => {
+		     gn => 'http://www.geonames.org/ontology#',
+		     owl => 'http://www.w3.org/2002/07/owl#',
+		     pos => 'http://www.w3.org/2003/01/geo/wgs84_pos#',
+		     gd => 'http://vocab.lenka.no/geo-deling#',
+		     cc => 'http://creativecommons.org/ns#',
+		     xsd => 'http://www.w3.org/2001/XMLSchema#',
+		     rdf => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+		    },
+      base_uri => 'http://data.lenka.no/geo/',
+      ExpandQNames => 1,
+   );
 
 my $rows = scraper {
   process "tr", "rows[]" => scraper {
@@ -26,15 +40,6 @@ my $rows = scraper {
 };
 
 my $res = $rows->scrape( URI->new("http://www.erikbolstad.no/geo/skandinavia/norske-kommunesenter/txt/") );
-
-print '@base <http://data.lenka.no/geo/> .' . "\n";
-
-print '@prefix gn: <http://www.geonames.org/ontology#> .' . "\n";
-print '@prefix owl: <http://www.w3.org/2002/07/owl#> .'. "\n";
-print '@prefix pos: <http://www.w3.org/2003/01/geo/wgs84_pos#> .' . "\n";
-print '@prefix gd: <http://vocab.lenka.no/geo-deling#> .' . "\n";
-print '@prefix cc: <http://creativecommons.org/ns#> .' . "\n";
-
 foreach my $row (@{$res->{rows}}) {
   next unless ($row->{admno} && $row->{ssr} );
   if (length($row->{fnr}) == 1) {
@@ -47,32 +52,38 @@ foreach my $row (@{$res->{rows}}) {
   $row->{lat} =~ s/\s//g;
   $row->{long} =~ s/\s//g;
 
-  print "\n<inndeling/$row->{fnr}/$row->{knr}> a gd:Kommune ;\n";
+  my $kommune = "inndeling/$row->{fnr}/$row->{knr}";
+  $rdf->assert_resource($kommune, 'rdf:type', 'gd:Kommune');
   if ($row->{kgeonames}) {
-    print "\towl:sameAs <http://sws.geonames.org/$row->{kgeonames}/> ;\n";
+    $rdf->assert_resource($kommune, 'owl:sameAs' , "http://sws.geonames.org/$row->{kgeonames}/");
   }
-  print "\tgn:parentFeature <inndeling/$row->{fnr}> ;\n";
-  print "\tgd:fylkenr \"$row->{fnr}\" ;\n";
-  print "\tgd:kommunenr \"$row->{knr}\" ;\n";
-  print "\tgd:senter <sted/$row->{ssr}>;\n";
-  print "\tpos:lat \"$row->{klat}\" ; \n";
-  print "\tpos:long \"$row->{klong}\" . \n";
-  print "\n<inndeling/$row->{fnr}> gn:childrenFeatures <inndeling/$row->{fnr}/$row->{knr}> .\n";
+  $rdf->assert_resource($kommune, 'gn:parentFeature', "inndeling/$row->{fnr}");
+  $rdf->assert_literal($kommune, 'gd:fylkenr', $row->{fnr});
+  $rdf->assert_literal($kommune, 'gd:kommunenr', $row->{knr});
+  $rdf->assert_resource($kommune, 'gd:senter', "sted/$row->{ssr}");
+  $rdf->assert_literal($kommune, 'pos:lat', $rdf->new_literal($row->{klat}, undef, 'xsd:decimal'));
+  $rdf->assert_literal($kommune, 'pos:long', $rdf->new_literal($row->{klong}, undef, 'xsd:decimal'));
 
-  print "\n<sted/$row->{ssr}> a gd:Kommunesenter ;\n";
-  print "\tgn:parentFeature <inndeling/$row->{fnr}/$row->{knr}> ;\n";
-  print "\tgn:parentADM1 <inndeling/$row->{fnr}> ;\n";
-  print "\tgn:parentADM2 <inndeling/$row->{fnr}/$row->{knr}> ;\n";
+  $rdf->assert_resource("inndeling/$row->{fnr}", 'gn:childrenFeatures', "inndeling/$row->{fnr}/$row->{knr}");
+
+  my $sted = "sted/$row->{ssr}";
+  $rdf->assert_resource($sted, 'rdf:type', 'http://vocab.lenka.no/geo-deling#Kommunesenter');
+  $rdf->assert_resource($sted, 'gn:parentFeature', "inndeling/$row->{fnr}/$row->{knr}");
+  $rdf->assert_resource($sted, 'gn:parentADM1', "inndeling/$row->{fnr}");
+  $rdf->assert_resource($sted, 'gn:parentADM2', "inndeling/$row->{fnr}/$row->{knr}");
   if ($row->{geonames}) {
-    print "\towl:sameAs <http://sws.geonames.org/$row->{geonames}/> ;\n";
+    $rdf->assert_resource($sted, 'owl:sameAs', "http://sws.geonames.org/$row->{geonames}/");
   }
-  print "\tpos:lat \"$row->{lat}\" ; \n";
-  print "\tpos:long \"$row->{long}\" ; \n";
-  print "\tgn:officialName \"$row->{admno}\"\@no .\n";
-
+  $rdf->assert_literal($sted, 'pos:lat', $rdf->new_literal($row->{lat}, undef, 'xsd:decimal'));
+  $rdf->assert_literal($sted, 'pos:long', $rdf->new_literal($row->{long}, undef, 'xsd:decimal'));
+  $rdf->assert_literal($sted, 'gn:officialName', $rdf->new_literal($row->{admno},  'no'));
 }
 
-print "\n</dumps/kommunesentre-geonames.ttl> a cc:Work ;\n";
-print "\tcc:license <http://creativecommons.org/licenses/by/3.0/> ;\n";
-print "\tcc:attributionName \"Erik Bolstad\" ;\n";
-print "\tcc:attributionURL <http://www.erikbolstad.no/geo/skandinavia/norske-kommunesenter/> .\n";
+my $dump = $rdf->get_object('/dumps/kommunesentre-geonames.ttl');
+
+$dump->rdf_type('http://creativecommons.org/ns#Work');
+$dump->cc_license('http://creativecommons.org/licenses/by/3.0/');
+$dump->cc_attributionName('Erik Bolstad');
+$dump->cc_attributionURL('http://www.erikbolstad.no/geo/skandinavia/norske-kommunesenter/');
+
+print $rdf->serialize(format => 'turtle')
